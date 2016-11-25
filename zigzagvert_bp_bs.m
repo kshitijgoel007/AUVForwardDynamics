ed7833a773b63f9fa272bab7031e129307928262
Kshitij <kshitijgoel16061995@gmail.com>
2016-11-26 03:25:19 +0530

    bp bs vertical zig zag fixed and verified

--- zigzagvert_bp_bs.m
+++ zigzagvert_bp_bs.m
@@ -1,12 +1,12 @@
 %script for testing zig-zag
-function zigzagvert_bp_bs()
-
-t=0:.1:100;
+function zigzagvert_bp_bs(tend)
+close all;
+t=0:.1:tend;
 dt=.1;
 global Y del_bp del_bs
 %MAT FILE CONTAING PROPERTIES OF BOT
 geoprop;                       
-r2d = 180*pi;
+r2d = 180/pi;
 X=zeros(18,1);
 X(1)=1; % initial surge velocity
 Y=zeros(length(t),length(X)+1);
@@ -34,6 +34,7 @@ del_bs=+10*pi/180.0;
     for j=1:length(X)
     Y(i,j+1)=X(j);
     end
+    
     %EULER METHOD IMPLEMENTED 
     %bot will turn opposite to control surface deflection
     if (X(11)>10*pi/180.0)
@@ -45,17 +46,14 @@ del_bs=+10*pi/180.0;
       del_bs=0*pi/180.0;
 %       % del_s=-10*pi/180.0;
         end
-   end
-   
-   Y(:,11) = Y(:,11)*(r2d);
-   Y(:,12) = Y(:,12)*(r2d);
-   Y(:,13) = Y(:,13)*(r2d);
+    end
    %plot(t,Y(:,11));
    end
-      figure(1);
-   subplot(3,1,1);plot(t,Y(:,12)*r2d);xlabel('time');ylabel('\theta');
-   subplot(3,1,2);plot(t,Y(:,11)*r2d);xlabel('time');ylabel('\phi');
-   subplot(3,1,3);plot(t,Y(:,13)*r2d);xlabel('time');ylabel('\psi');
+   
+   figure(1);
+   subplot(3,1,1);plot(t,Y(:,12)*r2d);xlabel('time');ylabel('\theta (in degrees)');
+   subplot(3,1,2);plot(t,Y(:,11)*r2d);xlabel('time');ylabel('\phi (in degrees)');
+   subplot(3,1,3);plot(t,Y(:,13)*r2d);xlabel('time');ylabel('\psi (in degrees)');
   saveas(figure(1),'E:\AUV\AUVForwardDynamics\Results\vert_zigzag_bp_bs\const_zero\euler.fig');
    
    figure(2);
@@ -65,7 +63,7 @@ del_bs=+10*pi/180.0;
    saveas(figure(2),'E:\AUV\AUVForwardDynamics\Results\vert_zigzag_bp_bs\const_zero\xyz.fig');
    
    figure(3);
-   subplot(2,1,1);plot(t, Y(:,14));xlabel('time');ylabel('\delta_{b_{s,actual}}');
-   subplot(2,1,2);plot(t, Y(:,14));xlabel('time');ylabel('\delta_{bp_{p,actual}}');
+   subplot(2,1,1);plot(t, Y(:,15));xlabel('time');ylabel('\delta_{b_{s,actual}}');
+   subplot(2,1,2);plot(t, Y(:,16));xlabel('time');ylabel('\delta_{b_{p,actual}}');
    saveas(figure(3),'E:\AUV\AUVForwardDynamics\Results\vert_zigzag_bp_bs\const_zero\delbsbp.fig');
 end
