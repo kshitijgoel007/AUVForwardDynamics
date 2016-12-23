de6780703034b3ae47414fd73ecd90dbf8a76745
Aniket Bhushan <aniket2701bhushan@gmail.com>
2016-12-23 20:26:50 +0530

    IMU model version1
    Created IMU model, generating linear acceleration and angular
    velocities.

--- sensorModels/accelerometer_model.m
+++ sensorModels/accelerometer_model.m
@@ -1,20 +1,32 @@
 % Three axis Accelerometer model - [22-12-2016]
+% https://github.com/ethz-asl/kalibr/wiki/IMU-Noise-Model
+% An introduction to inertial navigation by Oliver J. Woodman
 
-function a_meas = acceleration_measured(wb, wb_dot, Acc_true, tinc)
+function a_meas = accelerometer_model(wb, wb_dot, Acc_true, tinc)
+% ACCELEROMETER_MODEL generates measured acceleration data [m/s2]
+%  wb : Angular velocity of body expressed in body-frame [rad/s]
+%  wb_dot : Angular acceleration of body expressed in body-frame [rad/s2]
+%  tinc : time step, [IMPORTANT : Depends upon sampling rate in case of actual IMU]
+%  bias : previous value of bias
 
+global accelerometer_location;
+global r_cg;
+global accelerometer_bias_instability;
+global accelerometer_noise_density;
+global IMU_Accelerometer_SF_MA;
 
-d = [(Accelerometer_Location(1) -  r_cg(1));
-     (Accelerometer_Location(2) -  r_cg(2));
-     (Accelerometer_Location(3) -  r_cg(3))];
-
+d = [(accelerometer_location(1) -  r_cg(1));
+     (accelerometer_location(2) -  r_cg(2));
+     (accelerometer_location(3) -  r_cg(3))];
+ 
 Aimeas = Acc_true + cross(wb,cross(wb,d)) + cross(wb_dot,d);
 
-% Accelerometer bias signal
+% Accelerometer bias signal %
 % accelerometer_sig_beta = acceleration_random_walk*sqrt(tinc);
-% accelerometer_beta += accelero_sig_beta*randn(3,1);
-accelerometer_beta = accelerometer_bias_stability*randn(3,1);
+% accelerometer_beta += acceleration_sig_beta*randn(3,1);
+accelerometer_beta = accelerometer_bias_instability*randn(3,1);
 
-% Accelerometer white noise signal
+% Accelerometer white noise signal %
 accelerometer_noise_density_d = accelerometer_noise_density*(1/sqrt(tinc));
 accelerometer_white_noise = accelerometer_noise_density_d*randn(3,1);     %  White noise (m/s2)
 
--- sensorModels/gyro_model.m
+++ sensorModels/gyro_model.m
@@ -1,4 +1,13 @@
-function [WMeas,Bias] = gyr_model(WInput,BiasPrev,tinc)
+function WMeas = gyro_model(wb, tinc)
+% GYRO_MODEL generates measured angular velocity in [deg/s]
+%  wb : Angular velocity of body expressed in body-frame [rad/s]
+%  tinc : time step, [IMPORTANT : Depends upon sampling rate in case of actual IMU]
+
+global gyroscope_bias_instability;
+global gyroscope_noise_density;
+
+% convert wb from [rad/s] to [deg/s]
+wb = wb*180*(1/3.14);
 
 % Gyroscope bias signal %
 % gyroscope_sig_beta = gyroscope_random_walk*sqrt(tinc);
@@ -9,6 +18,6 @@ gyroscope_bias = gyroscope_bias_instability*randn(3,1);
 gyroscope_noise_density_d = gyroscope_noise_density*(1/sqrt(tinc));
 gyroscope_white_noise = gyroscope_noise_density_d*randn(3,1);   %  White noise (deg/s)
 
-WMeas = WInput + gyroscope_bias + gyroscope_white_noise;
+WMeas = wb + gyroscope_bias + gyroscope_white_noise;
 
 end
