18aa57a00c97c944e8600e8db9e84d460527c32c
Aniket Bhushan <aniket2701bhushan@gmail.com>
2017-03-22 16:46:17 +0530

    Main file to run state estimation from dataMatrix
    Signed-off-by: Aniket Bhushan <aniket2701bhushan@gmail.com>

---
+++ utils/main.m
@@ -0,0 +1,49 @@
+clc
+clear all;
+close all;
+
+%% setup
+addpath('datapoints');
+addpath('sensorModels');
+addpath('helperFunctions');
+addpath('stateEstimation');
+
+%% load simulation parameters & data
+A = csvread('GoodSensordata30min.csv',1);
+vel_bf = A(:,2:4)'; % m/s
+omega_bf = A(:,5:7)'*pi/180; % rad/s
+position_in = A(:,8:10)'; % m
+euler_angle = A(:,11:13)'*pi/180; % rad
+accel_bf = A(:,20:22)'; % m/s2
+omega_bf_dot = A(:,23:25)'*pi/180; % rad/s2
+
+X_true = [position_in;
+          euler_angle;
+          vel_bf;];
+
+% time parameters
+t = A(:,1);
+tinc = t(2) - t(1);
+
+%% load auv parameters as global params. [req. tinc]
+AUVsensors;
+EKFparams;
+
+% Estimated states [ pos, euler_angles, velocity] %
+X_est = zeros(size(X_true));
+P_est = zeros(size(X_true,2),9,9);
+
+%% Loop
+for i = 1:length(t)
+    
+   [X_est(:,i),P_est(i,:,:)] = stateEstimation(A(i,:), tinc);
+   
+   fprintf('t : %d \n', i*tinc);
+
+end
+
+
+eStates = (X_true - X_est)';
+timeVector = t;
+
+plotStateEstimData(timeVector, X_est, P_est, A);
